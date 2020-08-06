d2e372a4caf9dcdf0f5bb1e87ffbbb40939354e3
Erica Zhong <ezhong2@atlassian.com>
2020-08-06 16:47:27 -0700

    changes

--- start/kittenbot.js
+++ start/kittenbot.js
@@ -1,5 +1,5 @@
 /* *****************************************************************************
-Copyright 2016 Google LLC
+Copyright 2020 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License")
 you may not use this file except in compliance with the License.
@@ -17,51 +17,133 @@ limitations under the License.
 This is a sample Slack bot built with Botkit.
 */
 
-const { Botkit } = require('botkit')
-const { SlackAdapter, SlackEventMiddleware } = require(
-  'botbuilder-adapter-slack')
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
+const { Botkit, BotkitConversation } = require("botkit");
+const {
+  SlackAdapter,
+  SlackEventMiddleware,
+} = require("botbuilder-adapter-slack");
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
 
 /**
  * Returns the secret string from Google Cloud Secret Manager
  * @param {string} name The name of the secret.
  * @return {string} The string value of the secret.
  */
-async function accessSecretVersion (name) {
-  const client = new SecretManagerServiceClient()
-  const projectId = process.env.PROJECT_ID
+async function accessSecretVersion(name) {
+  const client = new SecretManagerServiceClient();
+  const projectId = process.env.PROJECT_ID;
   const [version] = await client.accessSecretVersion({
-    name: `projects/${projectId}/secrets/${name}/versions/1`
-  })
+    name: `projects/${projectId}/secrets/${name}/versions/1`,
+  });
 
   // Extract the payload as a string.
-  const payload = version.payload.data.toString('utf8')
+  const payload = version.payload.data.toString("utf8");
 
-  return payload
+  return payload;
 }
 
 /**
  * Asynchronous function to initialize kittenbot.
  */
-async function kittenbotInit () {
+async function kittenbotInit() {
   const adapter = new SlackAdapter({
-    clientSigningSecret: await accessSecretVersion('client-signing-secret'),
-    botToken: await accessSecretVersion('bot-token')
-  })
+    clientSigningSecret: await accessSecretVersion("client-signing-secret"),
+    botToken: await accessSecretVersion("bot-token"),
+  });
 
-  adapter.use(new SlackEventMiddleware())
+  adapter.use(new SlackEventMiddleware());
 
   const controller = new Botkit({
-    webhook_uri: '/api/messages',
-    adapter: adapter
-  })
+    webhook_uri: "/api/messages",
+    adapter: adapter,
+  });
 
+  // Add Kitten Dialog
+  const convo = createKittenDialog(controller);
+  controller.addDialog(convo);
+
+  // Controller is ready
   controller.ready(() => {
-    controller.hears(['hello', 'hi'], ['message', 'direct_message'],
+    // START: listen for cat emoji delivery
+    controller.hears(
+      ["Hello everyone! :zoomba:"],
+      ["message", "direct_message"],
       async (bot, message) => {
-        await bot.reply(message, 'Meow. :smile_cat:')
-      })
-  })
+        // Don't respond to self
+        if (message.bot_id !== message.user) {
+          numGoing = 0;
+          setTimeout(function () {
+            if (numGoing < threshhold) {
+              // Not enough people said yes in the timespan
+              await convo.gotoThread("no_zoom");
+            }
+          }, 10000); //600000
+          await bot.startConversationInChannel(message.channel, message.user);
+          return bot.beginDialog("kitten-delivery");
+        }
+      }
+    );
+    // END: listen for cat emoji delivery
+  });
 }
 
-kittenbotInit()
+const maxCats = 20;
+const threshhold = 3;
+let numGoing = 0;
+
+/**
+ * Function to create the kitten conversation
+ * @param {Object} controller The botkit controller.
+ * @return {Object} The BotkitConversation object.
+ */
+function createKittenDialog(controller) {
+  const convo = new BotkitConversation("kitten-delivery", controller);
+
+  convo.ask("Do you want to join a zoom room?", [
+    {
+      pattern: "yes",
+      handler: async (response, convo, bot) => {
+        numGoing++;
+        if (numGoing >= threshhold) {
+          await convo.gotoThread("yes_zoom");
+        }
+      },
+    },
+    {
+      pattern: "no",
+      handler: async (response, convo, bot) => {
+        await convo.gotoThread("no_zoom");
+      },
+    },
+    {
+      default: true,
+      handler: async (response, convo, bot) => {
+        await convo.gotoThread("no_zoom");
+      },
+    },
+  ]);
+
+  convo.addMessage(
+    {
+      text: "zoom link here!!!!!! https://atlassian.zoom.us/j/96856753417?pwd=QnppS2ovRXVtc2hJRG93ZjlqWklvUT09",
+      // attachments: [
+      //   {
+      //     fallback: "Chihuahua Bubbles - https://youtu.be/s84dBopsIe4",
+      //     text: "<https://youtu.be/s84dBopsIe4|" + "Chihuahua Bubbles>!",
+      //   },
+      // ],
+    },
+    "yes_zoom"
+  );
+
+  convo.addMessage(
+    {
+      text: "Not enough people are around... :sadpanda: Try again later!",
+    },
+    "no_zoom"
+  );
+
+  return convo;
+}
+
+kittenbotInit();
---
+++ start/kittenbot_old.js
@@ -0,0 +1,104 @@
+/* *****************************************************************************
+Copyright 2016 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License")
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ WE HAVE MODIFIED THIS FILE
+********************************************************************************
+
+This is a sample Slack bot built with Botkit.
+*/
+
+const { Botkit } = require("botkit");
+const {
+  SlackAdapter,
+  SlackEventMiddleware,
+} = require("botbuilder-adapter-slack");
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
+
+/**
+ * Returns the secret string from Google Cloud Secret Manager
+ * @param {string} name The name of the secret.
+ * @return {string} The string value of the secret.
+ */
+async function accessSecretVersion(name) {
+  const client = new SecretManagerServiceClient();
+  const projectId = process.env.PROJECT_ID;
+  const [version] = await client.accessSecretVersion({
+    name: `projects/${projectId}/secrets/${name}/versions/1`,
+  });
+
+  // Extract the payload as a string.
+  const payload = version.payload.data.toString("utf8");
+
+  return payload;
+}
+
+/**
+ * Asynchronous function to initialize kittenbot.
+ */
+async function kittenbotInit() {
+  const adapter = new SlackAdapter({
+    clientSigningSecret: await accessSecretVersion("client-signing-secret"),
+    botToken: await accessSecretVersion("bot-token"),
+  });
+
+  adapter.use(new SlackEventMiddleware());
+
+  const controller = new Botkit({
+    webhook_uri: "/api/messages",
+    adapter: adapter,
+  });
+
+  controller.ready(() => {
+    controller.hears(
+      ["hello", "hi"],
+      ["message", "direct_message"],
+      async (bot, message) => {
+        await bot.reply(message, "Meow. :smile_cat:");
+      }
+    );
+  });
+}
+
+kittenbotInit();
+
+new CronJob(
+  "0 */1 * * * *",
+  () => {
+    bot.api.im.open(
+      {
+        user: "U2WQ0L9FW",
+      },
+      (err, res) => {
+        if (err) {
+          bot.botkit.log("Failed to open IM with user", err);
+        }
+        console.log(res);
+        bot.startConversation(
+          {
+            user: "U2WQ0L9FW",
+            channel: res.channel.id,
+            text: "WOWZA... 1....2",
+          },
+          (err, convo) => {
+            convo.say("This is the shit");
+          }
+        );
+      }
+    );
+  },
+  null,
+  true,
+  "Africa/Lagos"
+);
