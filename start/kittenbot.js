b65a1cc77f19d2c761559bb804e42f947d56a8e8
Laurent Picard <PicardParis@users.noreply.github.com>
2023-05-23 18:09:19 +0200

    fix: allow direct gcloud run deploy w/o Dockerfile

--- start/kittenbot.js
+++ start/kittenbot.js
@@ -17,51 +17,56 @@ limitations under the License.
 This is a sample Slack bot built with Botkit.
 */
 
-const { Botkit } = require('botkit')
-const { SlackAdapter, SlackEventMiddleware } = require(
-  'botbuilder-adapter-slack')
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
+const { Botkit } = require('botkit');
+const {
+  SlackAdapter,
+  SlackEventMiddleware,
+} = require('botbuilder-adapter-slack');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 /**
  * Returns the secret string from Google Cloud Secret Manager
  * @param {string} name The name of the secret.
- * @return {string} The string value of the secret.
+ * @return {Promise<string>} The string value of the secret.
  */
-async function accessSecretVersion (name) {
-  const client = new SecretManagerServiceClient()
-  const projectId = process.env.PROJECT_ID
+async function accessSecretVersion(name) {
+  const client = new SecretManagerServiceClient();
+  const projectId = process.env.PROJECT_ID;
   const [version] = await client.accessSecretVersion({
-    name: `projects/${projectId}/secrets/${name}/versions/1`
-  })
+    name: `projects/${projectId}/secrets/${name}/versions/1`,
+  });
 
   // Extract the payload as a string.
-  const payload = version.payload.data.toString('utf8')
+  const payload = version.payload.data.toString('utf8');
 
-  return payload
+  return payload;
 }
 
 /**
- * Asynchronous function to initialize kittenbot.
+ * Function to initialize kittenbot.
  */
-async function kittenbotInit () {
+async function kittenbotInit() {
   const adapter = new SlackAdapter({
     clientSigningSecret: await accessSecretVersion('client-signing-secret'),
-    botToken: await accessSecretVersion('bot-token')
-  })
+    botToken: await accessSecretVersion('bot-token'),
+  });
 
-  adapter.use(new SlackEventMiddleware())
+  adapter.use(new SlackEventMiddleware());
 
   const controller = new Botkit({
     webhook_uri: '/api/messages',
-    adapter: adapter
-  })
+    adapter: adapter,
+  });
 
   controller.ready(() => {
-    controller.hears(['hello', 'hi'], ['message', 'direct_message'],
+    controller.hears(
+      ['hello', 'hi', 'hey'],
+      ['message', 'direct_message'],
       async (bot, message) => {
-        await bot.reply(message, 'Meow. :smile_cat:')
-      })
-  })
+        await bot.reply(message, 'Meow. :smile_cat:');
+      }
+    );
+  });
 }
 
-kittenbotInit()
+kittenbotInit();
